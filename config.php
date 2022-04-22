88c43e4ea0e3265c6187dd4138ccbcdf0d4be4b1
Keita Hirabayashi <100016499+keita-hirabayshi@users.noreply.github.com>
2022-04-22 23:55:17 +0900

    add commit
    追加のコミットです。

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+define('CURRENT_URI',$_SERVER['REQUEST_URI']);
+
+if(preg_match("/(.+(start|end))/i", CURRENT_URI, $match)) {
+    define('BASE_CONTEXT_PATH', $match[0] . '/');
+}
+define('BASE_IMAGE_PATH', BASE_CONTEXT_PATH . 'images/');
+define('BASE_JS_PATH', BASE_CONTEXT_PATH . 'js/');
+define('BASE_CSS_PATH', BASE_CONTEXT_PATH . 'css/');
+define('SOURCE_BASE', __DIR__ . '/php/');
